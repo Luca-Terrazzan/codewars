402fdfefebd8a355f7b08ff6260fdae39c417d2c
Luca Terrazzan <luca.terraz@gmail.com>
2020-08-20 17:02:47 +0200

    feat: improved solution

--- smallest_unused_id.py
+++ smallest_unused_id.py
@@ -1,17 +1,14 @@
 def next_id(arr: []):
-    if not arr:
-        return 0
-    # print(arr)
-    arr = list(set(arr))
-    arr.sort()
-    reference = list(range(max(arr) + 1))
-
-    diff = list(set(arr).symmetric_difference(reference))
-
-    return diff[0] if diff else arr[-1] + 1
+    return min(set(arr) ^ set(range(max(arr) + 2))) if arr else 0
 
 myarr = [3, 5, 5, 5, 7, 0, 4, 2, 11]
 print(next_id(myarr))
 
+myarr2 = set(range(max(myarr) + 2))
+print(set(myarr) ^ set(myarr2))
+
 myarr = []
+print(next_id(myarr))
+
+myarr = list(range(99999))
 print(next_id(myarr))
